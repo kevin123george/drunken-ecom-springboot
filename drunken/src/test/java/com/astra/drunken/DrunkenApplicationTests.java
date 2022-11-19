14fbcc24e316d7a56b6548da39572c76dc424240
Kevin <kevin.george@stud.uni-bamberg.de>
2022-11-19 11:33:06 +0100

    init project

---
+++ drunken/src/test/java/com/astra/drunken/DrunkenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astra.drunken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunkenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
