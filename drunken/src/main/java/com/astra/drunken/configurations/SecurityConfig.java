0d9396b5c257a2fe17ab2710201771456f8e717b
Kevin <kevin.george@stud.uni-bamberg.de>
2022-11-25 15:47:47 +0100

    ant macher for reg

--- drunken/src/main/java/com/astra/drunken/configurations/SecurityConfig.java
+++ drunken/src/main/java/com/astra/drunken/configurations/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(
-						"/registration**",
+						"/registration/",
 						"/js/**",
 						"/css/**",
 						"/img/**","/").permitAll()
