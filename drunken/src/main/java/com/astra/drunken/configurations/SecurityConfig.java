c1c44153bc93e17809aa0c4e6cfce224250f1c60
Kevin <kevin.george@stud.uni-bamberg.de>
2022-12-16 21:50:17 +0100

    added ant macher for search

--- drunken/src/main/java/com/astra/drunken/configurations/SecurityConfig.java
+++ drunken/src/main/java/com/astra/drunken/configurations/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                "/",
+                        "/",
+                        "/search/**",
                         "/user/*",
                         "/bottle/*",
                         "/bottle",
